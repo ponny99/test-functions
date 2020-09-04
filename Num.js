aa86870865a068d36c5f609557e8df2cd6e2f737
Selfmade20e <tebogo1686@gmail.com>
2020-09-04 12:22:40 +0200

    adding changes I made in the test

--- Num.js
+++ Num.js
@@ -1,14 +1,10 @@
 function telephoneCheck(str){
     
     let polishTelephone = str.replace()
-    console.log(polishTelephone);
-if (teleponeCheck.length === 10 ) 
     return true;
+    console.log(polishTelephone);
+if (teleponeCheck.length === 10 ) return true;
 
-} else if (polishTelephone.length === 11 && polishTelephone.length [0] === 1){
-    return true
-
-    return false;
 }
 
 console.log(telephoneCheck("079-243-6894"));
